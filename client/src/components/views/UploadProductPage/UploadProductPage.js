4668c6990aae4f209d1139c1d2116a292731eaf9
m-veloper <m_veloper@naver.com>
2021-10-31 02:48:57 +0900

    업로드 페이지 생성

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react'
-import { Typography, Button, Form, message, Input, Icon } from 'antd';
+import React, {useState} from 'react'
+import {Typography, Button, Form, message, Input, Icon} from 'antd';
 // import FileUpload from '../../utils/FileUpload'
 import Axios from 'axios';
 
-const { Title } = Typography;
-const { TextArea } = Input;
+const {Title} = Typography;
+const {TextArea} = Input;
 
 const Continents = [
-  { key: 1, value: "Africa" },
-  { key: 2, value: "Europe" },
-  { key: 3, value: "Asia" },
-  { key: 4, value: "North America" },
-  { key: 5, value: "South America" },
-  { key: 6, value: "Australia" },
-  { key: 7, value: "Antarctica" }
+  {key: 1, value: "Africa"},
+  {key: 2, value: "Europe"},
+  {key: 3, value: "Asia"},
+  {key: 4, value: "North America"},
+  {key: 5, value: "South America"},
+  {key: 6, value: "Australia"},
+  {key: 7, value: "Antarctica"}
 ]
 
 function UploadProductPage(props) {
@@ -76,56 +76,43 @@ function UploadProductPage(props) {
   }
 
   return (
-    <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
-      <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+    <div style={{maxWidth: '700px', margin: '2rem auto'}}>
+      <div style={{textAlign: 'center', marginBottom: '2rem'}}>
         <Title level={2}> Upload Travel Product</Title>
       </div>
 
 
-      <Form onSubmit={onSubmit} >
+      <Form onSubmit={onSubmit}>
 
         {/* DropZone */}
         {/*<FileUpload refreshFunction={updateImages} />*/}
 
-        <br />
-        <br />
+        <br/>
+        <br/>
         <label>Title</label>
-        <Input
-          onChange={onTitleChange}
-          value={TitleValue}
+        <Input onChange={onTitleChange} value={TitleValue}
         />
-        <br />
-        <br />
+        <br/>
+        <br/>
         <label>Description</label>
-        <TextArea
-          onChange={onDescriptionChange}
-          value={DescriptionValue}
-        />
-        <br />
-        <br />
+        <TextArea onChange={onDescriptionChange} value={DescriptionValue}/>
+        <br/>
+        <br/>
         <label>Price($)</label>
-        <Input
-          onChange={onPriceChange}
-          value={PriceValue}
-          type="number"
-        />
-        <br /><br />
+        <Input onChange={onPriceChange} value={PriceValue} type="number"/>
+        <br/><br/>
         <select onChange={onContinentsSelectChange} value={ContinentValue}>
           {Continents.map(item => (
             <option key={item.key} value={item.key}>{item.value} </option>
           ))}
         </select>
-        <br />
-        <br />
+        <br/>
+        <br/>
 
-        <Button
-          onClick={onSubmit}
-        >
+        <Button onClick={onSubmit}>
           Submit
         </Button>
-
       </Form>
-
     </div>
   )
 }
