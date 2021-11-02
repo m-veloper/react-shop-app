8ac1b8ce9a014b5eb815b61a869d74336e0cff04
m-veloper <m_veloper@naver.com>
2021-11-03 03:29:52 +0900

    Card로 데이터 화면 출력

--- server/routes/product.js
+++ server/routes/product.js
@@ -76,57 +76,66 @@ router.post("/uploadProduct", auth, (req, res) => {
   })
 });
 
+/**
+ * 상품목록 가져오기
+ */
+router.post("/getProducts", (req, res) => {
 
-// router.post("/getProducts", (req, res) => {
-//
-//   let order = req.body.order ? req.body.order : "desc";
-//   let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
-//   let limit = req.body.limit ? parseInt(req.body.limit) : 100;
-//   let skip = parseInt(req.body.skip);
-//
-//   let findArgs = {};
-//   let term = req.body.searchTerm;
-//
-//   for (let key in req.body.filters) {
-//
-//     if (req.body.filters[key].length > 0) {
-//       if (key === "price") {
-//         findArgs[key] = {
-//           $gte: req.body.filters[key][0],
-//           $lte: req.body.filters[key][1]
-//         }
-//       } else {
-//         findArgs[key] = req.body.filters[key];
-//       }
-//     }
-//   }
-//
-//   console.log(findArgs)
-//
-//   if (term) {
-//     Product.find(findArgs)
-//       .find({$text: {$search: term}})
-//       .populate("writer")
-//       .sort([[sortBy, order]])
-//       .skip(skip)
-//       .limit(limit)
-//       .exec((err, products) => {
-//         if (err) return res.status(400).json({success: false, err})
-//         res.status(200).json({success: true, products, postSize: products.length})
-//       })
-//   } else {
-//     Product.find(findArgs)
-//       .populate("writer")
-//       .sort([[sortBy, order]])
-//       .skip(skip)
-//       .limit(limit)
-//       .exec((err, products) => {
-//         if (err) return res.status(400).json({success: false, err})
-//         res.status(200).json({success: true, products, postSize: products.length})
-//       })
-//   }
-//
-// });
+  let order = req.body.order ? req.body.order : "desc";
+  let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+  let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+  let skip = parseInt(req.body.skip);
+
+  let findArgs = {};
+  let term = req.body.searchTerm;
+
+  // key : continents 또는 price
+  for (let key in req.body.filters) {
+
+    if (req.body.filters[key].length > 0) {
+      if (key === "price") {
+        findArgs[key] = {
+          // gte : greater than equal
+          // lte : less than equal
+          $gte: req.body.filters[key][0],
+          $lte: req.body.filters[key][1]
+        }
+      } else {
+        findArgs[key] = req.body.filters[key];
+      }
+    }
+  }
+
+  console.log(findArgs)
+
+  // populate : 작성자(writer)에대한 모든 정보를 가져온다
+  if (term) {
+    Product.find(findArgs)
+      .find({$text: {$search: term}})
+      .populate("writer")
+      .sort([[sortBy, order]])
+      .skip(skip)
+      .limit(limit)
+      .exec((err, products) => {
+        if (err) {
+          return res.status(400).json({success: false, err})
+        } else {
+          return res.status(200).json({success: true, products, postSize: products.length})
+        }
+      })
+  } else {
+    Product.find(findArgs)
+      .populate("writer")
+      .sort([[sortBy, order]])
+      .skip(skip)
+      .limit(limit)
+      .exec((err, products) => {
+        if (err) return res.status(400).json({success: false, err})
+        res.status(200).json({success: true, products, postSize: products.length})
+      })
+  }
+
+});
 
 
 //?id=${productId}&type=single
